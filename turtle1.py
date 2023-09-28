11e2b27fff7a5c284813bfab8b86cc52bac771c3
Bentley Born <bentley@crcibernetica.com>
2023-09-28 16:33:17 -0700

    update

---
+++ turtle1.py
@@ -0,0 +1,14 @@
+# Usar "Local Python 3" Interpreter
+
+import turtle
+
+t = turtle.Turtle()
+
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
