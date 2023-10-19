e7c3c4ca3701db79f60f03f5f30a02ccee4197cc
Bentley Born <bentley@crcibernetica.com>
2023-10-18 18:49:05 -0700

    Update condicionales.py

--- Sesion2/condicionales.py
+++ Sesion2/condicionales.py
@@ -10,7 +10,8 @@ pot = ib.AnalogIn(board.IO33)
 while True:
     valor = sw.value
     valor_pot = pot.value
-    if valor_pot > 30000:
+    print(valor, valor_pot)
+    if valor_pot > 30000 and not valor:
         ib.pixel = (0,255,0)
     else:
         ib.pixel = (0,0,0)
