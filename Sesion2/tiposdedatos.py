f1e03684b6a949bfe07637a7b66bf3f83f5eae08
Bentley Born <bentley@crcibernetica.com>
2023-10-18 19:17:26 -0700

    Update tiposdedatos.py

--- Sesion2/tiposdedatos.py
+++ Sesion2/tiposdedatos.py
@@ -1,1 +1,52 @@
-# Tipos de datos
+# Tipos de datos (integrados)
+
+# Booleanos
+x = False
+y = True
+
+# Entero (integers)
+z = 10
+print(type(z))
+
+# Decimal (float)
+w = 12.34
+
+# String (cadenas de caracteres)
+texto = "hola"
+texto2 = 'hola'
+
+# Lista
+animales = ["tigre", "leon", "leopardo", "pantera"]
+numeros = [1, 7, 9, 15, 10]
+loaquesea = [1, 5, "hola", [8,9,5]]
+
+# Iteraciones de una lista
+for animal in animales:
+    print(animal)
+
+# Iteraciones de una lista con indice
+for indice, animal in enumerate(animales):
+    print(indice, animal)
+
+# Tupla es un lista pero no se puede modificar
+coordinados = (10.5, -84.7, 931)
+print(coordinados)
+
+# Desempacar una Tupla
+latitud, longitud, altura = coordinados
+print(latitud)
+print(longitud)
+print(altura)
+
+# Diccionario
+estudiante = {
+    "nombre":"Jose",
+    "
+    }
+
+
+
+
+
+
+
