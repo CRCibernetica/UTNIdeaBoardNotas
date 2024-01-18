62f0fa50b990e69ddeb934da6f0906faea809716
Bentley Born <bentley@crcibernetica.com>
2024-01-17 16:46:42 -0800

    update sesion8

---
+++ Sesion8/secrets.py
@@ -0,0 +1,8 @@
+secrets = {
+    'ssid' : 'ap-ssid',
+    'password' : 'ap-password',
+    'broker' : 'broker.hivemq.com',
+    'port' : 1883,
+    'topic' : 'UTN/clase8',
+    'deviceid' : 'minombre'
+}
